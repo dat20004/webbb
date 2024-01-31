88b707ab07b88de8b61bd6e076fa1ee87922e307
dat20004 <nvdat22082004@gmail.com>
2024-01-31 22:56:10 +0700

    up

--- cv/index.js
+++ cv/index.js
@@ -17,3 +17,20 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 2000); // Thay đổi ảnh mỗi 2 giây (có thể điều chỉnh thời gian tại đây)
 }
+// ---------
+function myD() {
+  var x = document.getElementById("menu");
+  if (x.style.visibility === "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}
+function myX() {
+  var x = document.getElementById("menu");
+  if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+  } else {
+    x.style.visibility = "visible";
+  }
+}
